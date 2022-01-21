1655159bdf7fb3e2b198bb40ec2beec8df20d0fb
Dzerjinski Lemos <dzerjinski@yahoo.com>
2022-01-21 06:30:40 +0100

    Add documentation

--- pkg/coalesce/coalesce.go
+++ pkg/coalesce/coalesce.go
@@ -1,5 +1,6 @@
 package coalesce
 
+// Return the first NotNil element in the parameter list.
 func NotNil[T any](values ...T) T {
 	isNotNil := func(v T) bool { return interface{}(v) != nil }
 
@@ -10,6 +11,7 @@ type EmptyConstraint interface {
 	[]interface{} | ~string
 }
 
+// Return the first NotNil element in the parameter list.
 func NotEmpty[T EmptyConstraint](values ...T) T {
 
 	isNotEmpty := func(v T) bool { return len(v) > 0 }
@@ -17,6 +19,7 @@ func NotEmpty[T EmptyConstraint](values ...T) T {
 	return Func(isNotEmpty, values...)
 }
 
+// Return the first element in the parameter list that is equal to the given target.
 func Equal[T any](target T, values ...T) T {
 
 	isEqual := func(value T) bool {
@@ -26,6 +29,7 @@ func Equal[T any](target T, values ...T) T {
 	return Func(isEqual, values...)
 }
 
+// Return the first element in the parameter list that satisfies the given condition.
 func Func[T any](test func(T) bool, values ...T) T {
 
 	for _, v := range values {
